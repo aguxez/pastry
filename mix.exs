2b06d05f3bd0321363f6191718b7d02f12655dbc
aguxez <dizmiiguel@gmail.com>
2018-04-11 19:47:56 -0400

    added hex config

--- mix.exs
+++ mix.exs
@@ -4,10 +4,14 @@ defmodule Pastry.Mixfile do
   def project do
     [
       app: :pastry,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.5",
-      start_permanent: Mix.env == :prod,
-      deps: deps()
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      package: pkg(),
+      description: description(),
+      name: "Pastry",
+      source_url: "https://github.com/aguxez/pastry"
     ]
   end
 
@@ -22,8 +26,20 @@ defmodule Pastry.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev}
     ]
+  end
+
+  defp pkg do
+    [
+      files: ~w(lib mix.exs README*),
+      maintainers: ["Miguel Diaz"],
+      licenses: ["MIT"],
+      links: %{"Github" => "https://github.com/aguxez/pastry"}
+    ]
+  end
+
+  defp description do
+    "Turn maps and keyword lists into query strings"
   end
 end
